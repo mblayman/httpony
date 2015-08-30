97c58a6533dda0aa935428be023b0bcd60038a13
Matt Layman <matthewlayman@gmail.com>
2015-08-30 14:52:04 -0400

    Start a basic server.

---
+++ httpony/server.py
@@ -0,0 +1,24 @@
+import logging
+from wsgiref.simple_server import demo_app
+
+from werkzeug.serving import run_simple
+
+
+def main():
+    """Serve up some ponies."""
+    hostname = 'localhost'
+    port = 8000
+    print (
+        "Making all your dreams for a pony come true on http://{0}:{1}.\n"
+        "Press Ctrl+C to quit."
+    ).format(hostname, port)
+
+    # Hush, werkzeug.
+    logging.getLogger('werkzeug').setLevel(logging.CRITICAL)
+
+    # TODO: Stop using the demo app.
+    run_simple(hostname, port, demo_app)
+
+
+if __name__ == '__main__':
+    main()
