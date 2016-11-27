169f0df6d0d315a411ec1099473fc7ad4d0efd5c
Matt Layman <matthewlayman@gmail.com>
2016-11-26 22:00:49 -0500

    Add some parser tests. (#26)

--- httpony/server.py
+++ httpony/server.py
@@ -39,11 +39,12 @@ def parse(argv):
 
 def build_parser():
     """Build the parser that will have all available commands and options."""
-    description = 'HTTPony (pronounced aych-tee-tee-pony) is a simple HTTP ' \
-                  'server that pretty prints HTTP requests to a terminal. It' \
-                  ' is a useful aide for developing clients that send HTTP ' \
-                  'requests. HTTPony acts as a sink for a client so that a ' \
-                  'developer can understand what the client is sending.'
+    description = (
+        'HTTPony (pronounced aych-tee-tee-pony) is a simple HTTP '
+        'server that pretty prints HTTP requests to a terminal. It '
+        'is a useful aide for developing clients that send HTTP '
+        'requests. HTTPony acts as a sink for a client so that a '
+        'developer can understand what the client is sending.')
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument(
         '-l', '--listen', help='set the IP address or hostname',
---
+++ httpony/tests/test_parser.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2016, Matt Layman and contributors
+
+import pytest
+
+from httpony import server
+
+
+def make_argv(arguments=None):
+    """Make an argument list suitable for the parse function."""
+    argv = ['httpony']
+    if arguments:
+        argv.extend(arguments)
+    return argv
+
+
+def test_listen_default():
+    argv = make_argv()
+    args = server.parse(argv)
+    assert args.listen == 'localhost'
+
+
+def test_listen():
+    argv = make_argv(['--listen', '0.0.0.0'])
+    args = server.parse(argv)
+    assert args.listen == '0.0.0.0'
+
+
+def test_port_default():
+    argv = make_argv()
+    args = server.parse(argv)
+    assert args.port == 8000
+
+
+def test_port():
+    argv = make_argv(['--port', '8080'])
+    args = server.parse(argv)
+    assert args.port == 8080
+
+
+def test_invalid_port():
+    argv = make_argv(['--port', 'broken'])
+    with pytest.raises(SystemExit):
+        server.parse(argv)
